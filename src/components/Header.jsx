61f6c76cb02adb298d2cebfb0e487738be93d6b8
SipuliCodes <vepsalainensisu@gmail.com>
2024-01-19 22:06:42 +0200

    first version website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className='item1'>
-      <h1 className='header'>Play the roll</h1>
+      <h1 className='header'></h1>
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,15 @@
+import me_img from '../assets/me.png'
+
 const Main = () => {
   return (
-    <div className='item2'>
-      Second
+    <div className='item2 main-container'>
+      <div className='left-main'>
+        <h1 className='name'>Sisu <br/>Vepsäläinen</h1>
+        <p>Student at University of Turku</p>
+      </div>
+      <div>
+        <img src={me_img} width="240px"/>
+      </div>
     </div>
   )
 }
