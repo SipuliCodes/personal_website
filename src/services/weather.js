de509fc86d8d0cace6c0739018c5b24019342ff0
SipuliCodes <vepsalainensisu@gmail.com>
2024-01-22 21:54:47 +0200

    small fix

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const OPENWEATHER_API_KEY = import.meta.env.VITE_SOME_KEY
+const api_key = import.meta.env.VITE_OPENWEATHER
 
 
 const getAll = async city => {
-  const city_data = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${OPENWEATHER_API_KEY}`)
-  return await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${city_data.data[0].lat}&lon=${city_data.data[0].lon}&units=metric&appid=${OPENWEATHER_API_KEY}`)
+  const city_data = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`)
+  return await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${city_data.data[0].lat}&lon=${city_data.data[0].lon}&units=metric&appid=${api_key}`)
 }
 
 export default {getAll}
